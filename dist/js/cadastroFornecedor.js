01d70082028cde3b7973a020ede03b0727e65080
Duany Rocker <duuh31.rocker@gmail.com>
2021-05-08 13:40:55 -0300

    Validação de CNPJ das páginas de cadastro

--- dist/js/cadastroFornecedor.js
+++ dist/js/cadastroFornecedor.js
@@ -45,15 +45,78 @@ function validarCadastro() {
         alerta();
         return false;
     }
-    //CNPJ
-    let cnpj = frmCadastro.cnpj;
-    if (cnpj.value == "" ||
-        cnpj.value == null) {
-        cnpj.focus();
+
+    //email
+    let email = document.getElementById('email').value;
+    let regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+    if (email.value == "") {
+        alerta();
+        return false;
+    }
+    else if (!regex.test(email)) {
         alerta();
         return false;
     }
 
+    //CNPJ
+    let cnpj = document.getElementById("cnpj").value;
+    cnpj = cnpj.replace(/[^\d]+/g, '');
+    if (cnpj == '') {
+        alerta();
+        return false;
+    }
+
+    if (cnpj.length != 14) {
+        alerta();
+        return false;
+    }
+
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj == "00000000000000" ||
+        cnpj == "11111111111111" ||
+        cnpj == "22222222222222" ||
+        cnpj == "33333333333333" ||
+        cnpj == "44444444444444" ||
+        cnpj == "55555555555555" ||
+        cnpj == "66666666666666" ||
+        cnpj == "77777777777777" ||
+        cnpj == "88888888888888" ||
+        cnpj == "99999999999999") {
+        alerta();
+        return false;
+    }
+
+    // Valida DVs
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0, tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0)) {
+        alerta();
+        return false;
+    }
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1)) {
+        alerta();
+        return false;
+    }
+    
     //telefone
     let telefone = frmCadastro.telefone;
     if (telefone.value == "" || telefone.value == null) {
