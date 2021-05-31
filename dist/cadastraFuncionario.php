7c502b55df0b140afbe0bd3a0d215aded0485132
wesney17 <wesney.trajano@gmail.com>
2021-05-30 23:51:23 -0300

    função md5 funcionario

--- dist/cadastraFuncionario.php
+++ dist/cadastraFuncionario.php
@@ -26,12 +26,14 @@ if($_POST){
     $vdepartamento=$_POST["nomeDepartamento"];
     $vcelular=$_POST["celular"];
     $vsenha1=$_POST["senha"]; 
+    $vsenhaf1 = md5($vsenha1);
     $vsenha2=$_POST["senha2"]; 
+    $vsenhaf2 = md5($vsenha2);
 
     //-----------------------------VERIFICANDO SENHAS------------------------------------
     
 
-    if($vsenha1!=$vsenha2)
+    if($vsenhaf1!=$vsenhaf2)
     {
      echo ("<script>
      $(document).ready(function(){ 
@@ -83,10 +85,9 @@ if($_POST){
 
      $sql = $conn->prepare(" INSERT INTO TBL_FUNCIONARIO
      (CPF_FUN,NOME_FUN,EMAIL_FUN,SENHA_FUN,TELEFONE_MOVEL_FUN, COD_DEP)
-     VALUES
-     (?, ?, ?, ?, ?, ?) ");
+     VALUES (?, ?, ?, ?, ?, ?) ");
 
-     $sql -> bind_param("ssssss", $vcpf,$vnome,$vmail,$vsenha1,$vcelular,$vdepartamento);
+     $sql -> bind_param("ssssss", $vcpf,$vnome,$vmail,$vsenhaf1,$vcelular,$vdepartamento);
 
      //-----------------------REALIZA O CADASTRO DOS DADOS NO BANCO TBL_CONTATO ----------------------
 
--- dist/verificaloginFuncionario.php
+++ dist/verificaloginFuncionario.php
@@ -6,9 +6,11 @@ include('conexao.php');
 //Verifica se não é sql injection
 $Email = mysqli_real_escape_string($conn, $_POST['email']);
 $Senha = mysqli_real_escape_string($conn, $_POST['senha']);
+$vsenha = md5($Senha);
+
 
 //Verifica se tem cadastro no banco de dados
-$Query = "SELECT COD_FUN, EMAIL_FUN FROM TBL_FUNCIONARIO WHERE EMAIL_FUN = '{$Email}' AND SENHA_FUN = '{$Senha}'";
+$Query = "SELECT COD_FUN, EMAIL_FUN FROM TBL_FUNCIONARIO WHERE EMAIL_FUN = '{$Email}' AND SENHA_FUN = '{$vsenha}'";
 
 $result = mysqli_query($conn, $Query);
 
