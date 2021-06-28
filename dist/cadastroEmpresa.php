013282d1e10e1486e6fe0b6ad250490a4fa4fe79
Duany Rocker <duuh31.rocker@gmail.com>
2021-06-27 23:25:56 -0300

    Atualização front-end Cadastro da Empresa

--- dist/cadastroEmpresa.php
+++ dist/cadastroEmpresa.php
@@ -12,18 +12,16 @@
     <!-- Icons -->
     <link rel="stylesheet" href="../assets/vendor/nucleo/css/nucleo.css" type="text/css">
     <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
-    <!-- Argon CSS -->
+    <!-- CSS -->
     <link rel="stylesheet" href="../assets/css/argon.css?v=1.2.0" type="text/css">
-    <!-- alerta css -->
+    <link rel="stylesheet" href="./css/validar.css" type="text/css">
+    <!-- Ajax -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+    <!-- Bootstrap -->
+     <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- Sweetalert -->
     <script src="./js/sweetalert.js"></script>
-    <style>
-       .error{
-             color:red
-       }
-</style>
 </head>
 
 <body>
@@ -275,15 +273,13 @@
     <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
     <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <!-- Puxando o jquery e plugin "mask" do jquery -->
-    <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+    <!-- Puxando o jquery,jquery validate e plugin "mask"-->
     <script type="text/javascript" src="js/jquery.mask.min.js"></script>
     <script type="text/javascript" src="js/jquery.validate.min.js"></script>
     <script type="text/javascript" src="js/localization/messages_pt_BR.js"></script>
     <script type="text/javascript" src="js/additional-methods.min.js"></script>
     <!-- JS -->
     <script src="./js/cadastroEmpresa.js"></script>
-
 </body>
 
 </html>
